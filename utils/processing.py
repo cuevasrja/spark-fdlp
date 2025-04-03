b9e38c7fa70295cee8af33c47dc8339ce3f1473e
Juan Cuevas <19-10056@usb.ve>
2025-04-02 22:59:39 -0400

    Modifica la función de casting en processing.py para incluir nuevos tipos de datos en las columnas del DataFrame, asegurando que se conviertan a los tipos adecuados.

--- utils/processing.py
+++ utils/processing.py
@@ -36,18 +36,68 @@ def casting(df: pd.DataFrame) -> pd.DataFrame:
     # acousticness,
     # energy,
     # loudness,
-    # instrumentalness
+    # instrumentalness,
+    # danceability,
+    # liveness,
+    # speechiness,
+    # valence,
+    # tempo
     df['acousticness'] = df['acousticness'].astype(float)
     df['energy'] = df['energy'].astype(float)
     df['loudness'] = df['loudness'].astype(float)
     df['instrumentalness'] = df['instrumentalness'].astype(float)
+    df['danceability'] = df['danceability'].astype(float)
+    df['liveness'] = df['liveness'].astype(float)
+    df['speechiness'] = df['speechiness'].astype(float)
+    df['valence'] = df['valence'].astype(float)
+    df['tempo'] = df['tempo'].astype(float)
 
     # Cast columns to int:
     # album_popularity,
-    # artist_popularity
-    # popularity
+    # artist_popularity,
+    # popularity,
+    # duration,
+    # explicit,
+    # key,
+    # mode,
+    # time_signature,
+    # followers,
+    # track_number,
+    # disc_number,
+    # is_playable
     df['album_popularity'] = df['album_popularity'].astype(int)
     df['artist_popularity'] = df['artist_popularity'].astype(int)
     df['popularity'] = df['popularity'].astype(int)
+    df['duration'] = df['duration'].astype(int)
+    df['explicit'] = df['explicit'].astype(int)
+    df['key'] = df['key'].astype(int)
+    df['mode'] = df['mode'].astype(int)
+    df['time_signature'] = df['time_signature'].astype(int)
+    df['followers'] = df['followers'].astype(int)
+    df['track_number'] = df['track_number'].astype(int)
+    df['disc_number'] = df['disc_number'].astype(int)
+    df['is_playable'] = df['is_playable'].astype(int)
+
+    # Cast columns to str:
+    # id,
+    # track_name,
+    # artist_name,
+    # genre_id,
+    # audio_feature_id,
+    # preview_url,
+    # album_name.
+    # album_group,
+    # album_type,
+    # release_date,
+    df['id'] = df['id'].astype(str)
+    df['track_name'] = df['track_name'].astype(str)
+    df['artist_name'] = df['artist_name'].astype(str)
+    df['genre_id'] = df['genre_id'].astype(str)
+    df['audio_feature_id'] = df['audio_feature_id'].astype(str)
+    df['preview_url'] = df['preview_url'].astype(str)
+    df['album_name'] = df['album_name'].astype(str)
+    df['album_group'] = df['album_group'].astype(str)
+    df['album_type'] = df['album_type'].astype(str)
+    df['release_date'] = df['release_date'].astype(str)
 
     return df
